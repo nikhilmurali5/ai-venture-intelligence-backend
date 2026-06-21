c4ff2aae8893cab23eb90e4d84c2328a40eba234
nikhilmurali5 <nikhilmuruali@gmail.com>
2026-06-21 23:21:01 +0530

    Fix CORS for Vercel

--- app/main.py
+++ app/main.py
@@ -38,7 +38,10 @@ import app.models
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://ai-venture-intelligence-frontend.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
